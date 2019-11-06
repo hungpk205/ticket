8ae220b86828c64b3d7895465edec626e0cea83a
HACHI <phankieuhunght205@gmail.com>
2019-11-06 10:30:05 +0700

    Manager booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,28 @@
 class BookingsController < ApplicationController
+  load_and_authorize_resource
+  before_action :load_company
+
+  def index
+    @bookings = @company.trips.bookings
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
